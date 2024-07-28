ad0b509ff6d63d8b2decf3e4cf5cf5571c84d4de
vail131 <vail131@naver.com>
2024-07-28 09:46:22 +0900

    [#15] airflow baseline - update airflow plugin. removing initiating Trader instacne

--- airflow/dags/kis_api_key_update_dag.py
+++ airflow/dags/kis_api_key_update_dag.py
@@ -1,22 +1,23 @@
-import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-
-from airflow.plugins.kis_key_update import update_kis_key
-
-key_update_dag = DAG(
-    # DAG 식별자용 아이디
-    dag_id="kis_key_update_dag",
-    description="update KIS(한투) api key and carve at python environment",
-    start_date=datetime.datetime.now(),
-    # 매일 00:00에 실행합니다
-    schedule_interval="0 0 * * *",
-)
-
-update_key = PythonOperator(
-    task_id="update_kis_key",
-    python_callable=update_kis_key,
-    dag=key_update_dag,
-)
-
-update_key
+# import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+#
+# from airflow.plugins.kis_key_update import update_kis_key
+#
+# TODO: Trader API활용하는 형태로 변경해야함
+# key_update_dag = DAG(
+#     # DAG 식별자용 아이디
+#     dag_id="kis_key_update_dag",
+#     description="update KIS(한투) api key and carve at python environment",
+#     start_date=datetime.datetime.now(),
+#     # 매일 00:00에 실행합니다
+#     schedule_interval="0 0 * * *",
+# )
+#
+# update_key = PythonOperator(
+#     task_id="update_kis_key",
+#     python_callable=update_kis_key,
+#     dag=key_update_dag,
+# )
+#
+# update_key
