36fe0fbbe41bd726c5a27e380afb2ec022481f3a
vail131 <vail131@naver.com>
2024-07-31 06:23:55 +0900

    [#20] add check n distribute dag

--- airflow/dags/asset_check_n_distribution_dag.py
+++ airflow/dags/asset_check_n_distribution_dag.py
@@ -1,11 +1,10 @@
-import os
 import datetime
 import logging
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from asset_update import check_balance, check_portfolio, get_run_task
+from asset_update import check_balance, check_portfolio, distribute_asset
 
 # Configure logger.py correctly
 # Logger
@@ -36,14 +35,20 @@ check_portfolio = BranchPythonOperator(
     dag=asset_check_n_distribution_dag,
 )
 
+distribute_asset = PythonOperator(
+    task_id="distribute_asset",
+    python_callable=distribute_asset,
+    provide_context=True,
+    dag=asset_check_n_distribution_dag,
+)
+
 task_empty = EmptyOperator(
     task_id='task_empty',
     dag=asset_check_n_distribution_dag
 )
 
 # balance를 받아서 env에 저장된 금액을 충족했는지 확인하고 만족했다면 portfolio에 가용한 금액인 accum_asset을 갱신해준다.
-
-# 예수금 확인 -> 분배 종목 반환
+# 예수금 확인 -> 분배 종목 리스트 반환
 check_balance >> [check_portfolio, task_empty]
-# 예수금 분배
+# # 미할당 포트폴리오에 자산 분배
 check_portfolio >> [distribute_asset, task_empty]
