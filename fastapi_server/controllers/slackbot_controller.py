c8f4dd93a981be13efe8fafb8f6d6897868d21a9
tofulim <vail131@naver.com>
2024-12-19 16:51:58 +0900

    [#54] add slack msg sending router DTO

--- fastapi_server/controllers/slackbot_controller.py
+++ fastapi_server/controllers/slackbot_controller.py
@@ -3,6 +3,7 @@ import inject
 
 from fastapi import APIRouter, Request
 from fastapi_server.slack import KISSlackBot
+from fastapi_server.entity.slack_base import SlackBase
 
 
 router = APIRouter(
@@ -17,13 +18,13 @@ logger = logging.getLogger("api_logger")
 
 
 @router.post("/send_message")
-async def prophet(request: Request, input_text: str, channel_id: str):
+async def prophet(request: Request, slack_base: SlackBase):
     slack_bot.post_message(
-        channel_id=channel_id,
-        text=input_text,
+        channel_id=slack_base.channel_id,
+        text=slack_base.input_text,
     )
     logger.inform(
-        input_text,
+        slack_base.input_text,
         extra={"endpoint_name": request.url.path}
     )
 
