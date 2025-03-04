9e1ea3c72ee4499ef6123bccb45bce6e84055792
tofulim <vail131@naver.com>
2025-03-04 13:03:29 +0900

    [#48] change image upload method(post_file) because it will be deprecated by 3.11

--- fastapi_server/slack.py
+++ fastapi_server/slack.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Union
+import requests
+from io import BytesIO
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -32,7 +33,8 @@ class KISSlackBot:
         except SlackApiError as e:
             logger.error(f"Error posting message: {e}")
 
-    def post_file(
+    # 3.11 자로 slack side deprecated
+    def deprecated_post_file(
         self,
         save_path: str,
         thread_ts: str = None,
@@ -52,11 +54,65 @@ class KISSlackBot:
         except SlackApiError as e:
             logger.error(f"Error posting message: {e}")
 
+    def post_file(
+        self,
+        file_path: str,
+        filename: str,
+        channel_id: str,
+        thread_ts: str = None,
+    ):
+        # ID of the channel you want to send the message to
+        file_stream = BytesIO()
+        with open(file_path, "rb") as f:
+            file_stream.write(f.read())
+
+        file_stream.seek(0, 2)  # 파일 끝으로 이동하여 크기 확인
+        length = file_stream.tell()
+        file_stream.seek(0)  # 다시 처음으로 이동
+
+        try:
+            # 1. 파일 업로드할 url 받기
+            result = self.client.files_getUploadURLExternal(
+                filename=filename,
+                length=length,
+            )
+            logger.info(result)
+
+            upload_url_data = result
+            upload_url = upload_url_data["upload_url"]
+            file_id = upload_url_data["file_id"]
+            # 2. 업로드 URL로 이미지 업로드
+            requests.post(
+                upload_url,
+                files={'file': (filename, file_stream, "image/jpeg")}  # MIME 타입 설정 (JPG/PNG 등)
+            )
+
+            # 3. 업로드 완료 요청
+            self.client.files_completeUploadExternal(
+                files=[
+                    {"id": file_id, "title": filename}
+                ],
+                channel_id=channel_id,
+                thread_ts=thread_ts,
+            )
+
+            return result
+
+        except SlackApiError as e:
+            logger.error(f"Error posting message: {e}")
+
 
 if __name__ == "__main__":
     slackbot = KISSlackBot(slack_bot_token="xoxb-...")
-    slackbot.post_message(
-        channel_id="test",
-        # thread_ts="1722946045.578359",
-        text="on test",
+    # slackbot.post_message(
+    #     channel_id="test",
+    #     # thread_ts="1722946045.578359",
+    #     text="on test",
+    # )
+
+    slackbot.post_file(
+        file_path = "/Users/limdohoon/PycharmProjects/auto-trade/test.png",
+        filename="test_file",
+        channel_id="C08FRRB60Q6",
+        # thread_ts=
     )
