6d4d7e1529d0ade2fddac6d78f22152f44019cf0
tofulim <vail131@naver.com>
2025-05-16 13:11:54 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 echo "AIRFLOW_UID=$(id -u)" > .env
 echo "AIRFLOW_GID=$(id -g)" >> .env
 
+mkdir airflow
+mkdir shared
+
+
 tmux kill-session -t "auto-trade" || true
 tmux new -d -s "auto-trade" docker compose up --build
