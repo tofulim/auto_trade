3724669f3ecbef3925650ad298cac72002e3a0da
tofulim <vail131@naver.com>
2025-05-16 12:54:48 +0900

    remove annoying chown logic

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-docker compose up airflow-init
-sudo chown -R "${AIRFLOW_UID}:0" ./airflow
+echo "AIRFLOW_UID=$(id -u)" > .env
+echo "AIRFLOW_GID=$(id -g)" >> .env
+
 tmux kill-session -t "auto-trade" || true
 tmux new -d -s "auto-trade" docker compose up --build
